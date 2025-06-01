e1d6a9a18e74291d06be368a3003137c10b0223d
GunnarDearringer <gunnar.dearringer@gmail.com>
2025-05-31 22:06:04 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("form").forEach(form => {
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        alert("Message sent! We'll get back to you soon.");
+    });
+});
