aeccbf7cb4271d0c8a558b1de7e461fc9e3a313d
GunnarDearringer <gunnar.dearringer@gmail.com>
2025-06-14 16:13:50 -0400

    Update affiliate links

--- items.js
+++ items.js
@@ -2,25 +2,25 @@ const items = [
     {
         name: "Everlast Pro Style Training Gloves",
         image: "https://images-na.ssl-images-amazon.com/images/I/71D1CJeTqrL._AC_SL1500_.jpg",
-        link: "https://www.amazon.com/dp/B001LUNUBO?tag=YOUR_AFFILIATE_TAG",
+        link: "https://amzn.to/44cDdbz",
         description: "Classic training gloves ideal for bag work and sparring."
     },
     {
         name: "Ringside Apex Bag Gloves",
         image: "https://images-na.ssl-images-amazon.com/images/I/81gVmJvSMZL._AC_SL1500_.jpg",
-        link: "https://www.amazon.com/dp/B00HY9NRA0?tag=YOUR_AFFILIATE_TAG",
+        link: "https://amzn.to/3FDZqpJ",
         description: "Durable synthetic leather gloves with vented palm."
     },
     {
         name: "TITLE Boxing Hand Wraps",
         image: "https://images-na.ssl-images-amazon.com/images/I/61k2XNnG8yL._AC_SL1500_.jpg",
-        link: "https://www.amazon.com/dp/B0012QGN12?tag=YOUR_AFFILIATE_TAG",
+        link: "https://amzn.to/3G42lrE",
         description: "180-inch hand wraps for added wrist support."
     },
     {
         name: "Cleto Reyes Hook and Loop Training Gloves",
         image: "https://images-na.ssl-images-amazon.com/images/I/71++9SWuLpL._AC_SL1500_.jpg",
-        link: "https://www.amazon.com/dp/B00LYFP7QI?tag=YOUR_AFFILIATE_TAG",
+        link: "https://amzn.to/3TmGWNo",
         description: "High-end leather gloves loved by professionals."
     }
 ];
